7bfa43794eda73ca4efe0a518ab7655bda4a348a
Dorcas <dorcaskomane1@gmail.com>
2023-07-28 22:17:14 +0200

    created the js function to render content dynamically on the component.

--- script.js
+++ script.js
@@ -1,25 +1,47 @@
-// fetch('./data.json')
-//     .then(res => res.json())
-//     .then(data => {
-//         console.log(data);
-//     });
 
-let icon = document.querySelector('#icon');
-let category = document.querySelector('#item');
-let itemScore = document.querySelector('#itemScore');
+// Function to fetch JSON data and content from each object 
+async function displaySummaryItems() {
+    try {
+      // Fetch the JSON file using Fetch API
+      const response = await fetch('./data.json');
+      const data = await response.json();
+  
+      // Get the container div where we will display the subject marks
+      const container = document.getElementById('right-side');
+    //   const categoryDiv = document.getElementById('item');
+  
+      // Loop through each object and create a div for it
+      for(let i = 0; i < data.length; i++){
 
+        const summaryItemDiv = document.createElement('div');
+        summaryItemDiv.classList.add('summary-item');
+        container.appendChild(summaryItemDiv);
 
-fetch('./data.json')
-    .then(res => res.json())
-    .then(data => {
-        console.log(data);
+        const itemTitleDiv = document.createElement('div');
+        itemTitleDiv.classList.add('item-title');
+        summaryItemDiv.appendChild(itemTitleDiv);
+        
+        const icon = document.createElement('img');
+        icon.classList.add('icon');
+        icon.src = `${data[i].icon}`;
+        itemTitleDiv.appendChild(icon);
 
-        for(let i = 0; i < data.length; i++){
-            icon.src = data[i].icon;
-            category.innerText = data[i].category;
-            itemScore.innerText = data[i].score;
-        }
-    })
-    .catch( err => {
-        console.log(`error ${err}`);
-    });
+        const categoryDiv = document.createElement('div');
+        categoryDiv.classList.add('item');
+        categoryDiv.classList.add('summary-item-name');
+        categoryDiv.innerHTML = `${data[i].category}`;
+        itemTitleDiv.appendChild(categoryDiv);
+
+        const scoreDiv = document.createElement('div');
+        scoreDiv.classList.add('itemScore');
+        scoreDiv.innerHTML = `${data[i].score} / 100`;
+        container.appendChild(scoreDiv);
+      }
+
+    } catch (error) {
+      console.error('Error fetching or displaying data:', error);
+    }
+  }
+  
+  // Call the function to display the subject marks when the page loads
+  displaySummaryItems(); 
