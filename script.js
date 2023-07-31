a18f422c6b92904105af3f9f93f91a137db395e0
Dorcas <dorcaskomane1@gmail.com>
2023-07-31 12:30:49 +0200

    Added code to display left-side content dynamically.

--- script.js
+++ script.js
@@ -6,16 +6,21 @@ async function displaySummaryItems() {
       const data = await response.json();
   
       // Get the container div where we will display the summary items
-      const container = document.getElementById('right-side');
-    //   const categoryDiv = document.getElementById('item');
+      const rightContainer = document.getElementById('right-side');
+      const leftContainer = document.getElementById('left-side');
+
+      //set up variables to be used to calculate final result
+      let sum = 0;
   
-      // Loop through each object and create a div for it
+      // Loop through each object and create the necessary divs for its contents 
       for(let i = 0; i < data.length; i++){
+
+        sum += data[i].score;
 
         const summaryItemDiv = document.createElement('div');
         summaryItemDiv.classList.add('summary-item');
         summaryItemDiv.style.background = `${data[i].background}`;
-        container.appendChild(summaryItemDiv);
+        rightContainer.appendChild(summaryItemDiv);
 
         const itemTitleDiv = document.createElement('div');
         itemTitleDiv.classList.add('item-title');
@@ -53,13 +58,39 @@ async function displaySummaryItems() {
       const buttonDiv = document.createElement('button');
       buttonDiv.classList.add('btn');
       buttonDiv.innerHTML = `Continue`;
-      container.appendChild(buttonDiv);
+      rightContainer.appendChild(buttonDiv);
 
       const summaryHeading = document.createElement('h2');
-      summaryHeading.textContent = 'Summary';
-      container.insertBefore(summaryHeading, container.firstElementChild);
+      summaryHeading.textContent = `Summary`;
+      rightContainer.insertBefore(summaryHeading, rightContainer.firstElementChild);
 
+      // LEFT-SIDE
+      const resultHeading = document.createElement('h1');
+      resultHeading.textContent = `Your Result`;
+      leftContainer.insertBefore(resultHeading, leftContainer.firstElementChild);
 
+      const resultDiv = document.createElement('div');
+      resultDiv.classList.add('middle');
+      leftContainer.appendChild(resultDiv);
+
+      const scoreSpan = document.createElement('span');
+      scoreSpan.classList.add('score');
+      scoreSpan.innerHTML = `${Math.floor(sum / data.length)}`;
+      resultDiv.appendChild(scoreSpan);
+
+      const subTextSpan = document.createElement('span');
+      subTextSpan.classList.add('sub-text');
+      subTextSpan.innerHTML = `of 100`;
+      resultDiv.appendChild(subTextSpan);
+
+      const bottomTitleSpan = document.createElement('span');
+      bottomTitleSpan.classList.add('bottom-title');
+      bottomTitleSpan.innerHTML = `Great`;
+      leftContainer.appendChild(bottomTitleSpan);
+
+      const par = document.createElement('p');
+      par.innerHTML = `You scored higher than 65% of the people who have taken these tests.`;
+      leftContainer.appendChild(par);
 
     } catch (error) {
       console.error('Error fetching or displaying data:', error);
